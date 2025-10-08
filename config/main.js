52b4485d1588288ac94c9f29704f49d95be0c07e
batmanhecker <513225@students.cumberlandschools.org>
2025-10-08 12:59:14 -0400

    main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "McCourt game website."; // Change this to change the name of your website.
+var subtext = "made by sivel shhhh!"; // set the subtext
 
 // more settings in main.css
 
